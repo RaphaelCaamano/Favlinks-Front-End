47567a23f2e0b647337b2f3e0c59ff08cbc08ccf
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-17 23:17:33 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express') //importing the express function from the express
+const app = express() // we're setting a new variable called app to be an express
+
+const port = 3000 // defining the port we want our app to run on
+
+// add some middleware to allow us to work with JSON data
+app.use(express.json())  
+
+// example route for the server
+app.get("/", (req, res)=>{
+
+    res.send("Node / Express server is operational!")
+})
+
+app.get("/something/different", (req, res)=>{-
+}) 
+
+let favLinks = [ 
+]
+
+// CRUD API 
+// API function 1 create something 
+app.post("/favlink", (req, res){-
+}) 
