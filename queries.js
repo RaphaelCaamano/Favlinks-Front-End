9c66b759201b5ee753e5473fc56e7ad72997dd3b
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-17 23:34:29 -0400

    Update queries.js

--- queries.js
+++ queries.js
@@ -1,12 +1,14 @@
+require('dotenv').config() // this exposes our env variables to our app!
+
 // Step 1) Establish a connection to our Postgres database 
 
 const Pool = require('pg'). Pool // a connection object to Postgres 
 
 const pool = new Pool({
 
-    user: 'me',
-    host: 'losthost',
-    database: 'favlinksapi',
-    password: '', 
-    port: 5432 
+    user: process.env.DB_USERNAME, 
+    host: process.env.DB_HOST, 
+    database: process.env.DB_NAME, 
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_HOST  
 })
